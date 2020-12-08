dd271c20b576ac016d0ececd9f0207ba67cb365f
rafaelthosi <thhrafael@outlook.com>
2020-12-08 14:07:35 +0000

    cor e filtro em todas
    Change-Id: Ic6d02d6aaf29a96223ac9ae54ba640abd44a130f

--- webapp/controller/Page1.controller.js
+++ webapp/controller/Page1.controller.js
@@ -3,12 +3,84 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 	"./TesteDialog",
 	"./utilities",
 	"sap/ui/core/routing/History",
-	'sap/ui/model/json/JSONModel'
-], function(BaseController, MessageBox, TesteDialog, Utilities, History, JSONModel) {
+	'sap/ui/model/json/JSONModel',
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (BaseController, MessageBox, TesteDialog, Utilities, History, JSONModel, Filter, FilterOperator) {
 	"use strict";
 
 	return BaseController.extend("com.sap.build.standard.veterinaria.controller.Page1", {
-		handleRouteMatched: function(oEvent) {
+		// FILTROS
+			// DONOS
+				onFilterInvoices : function (oEvent) {
+				
+					// build filter array
+					var aFilter = [];
+					var sQuery = oEvent.getParameter("query")
+		
+					if (sQuery) {
+						aFilter.push(new Filter("name", FilterOperator.Contains, sQuery));
+					}
+					
+					
+					// filter binding
+					var oList = this.byId("tableDonos");
+					var oBinding = oList.getBinding("items");
+					oBinding.filter(aFilter);
+				},
+				
+			//ANIMAIS
+				onFilterInvoices2 : function (oEvent) {
+					// build filter array
+					var aFilter = [];
+					var sQuery = oEvent.getParameter("query")
+		
+					if (sQuery) {
+						aFilter.push(new Filter("name", FilterOperator.Contains, sQuery));
+					}
+					
+					
+					// filter binding
+					var oList = this.byId("tableAnimais");
+					var oBinding = oList.getBinding("items");
+					oBinding.filter(aFilter);
+				},
+			
+			//CONSULTAS
+				onFilterInvoices3 : function (oEvent) {
+					// build filter array
+					var aFilter = [];
+					var sQuery = oEvent.getParameter("query")
+		
+					if (sQuery) {
+						aFilter.push(new Filter("status", FilterOperator.Contains, sQuery));
+					}
+					
+					
+					// filter binding
+					var oList = this.byId("tableConsultas");
+					var oBinding = oList.getBinding("items");
+					oBinding.filter(aFilter);
+				},
+				
+			//VETERINÁRIOS
+				onFilterInvoices4 : function (oEvent) {
+					// build filter array
+					var aFilter = [];
+					var sQuery = oEvent.getParameter("query")
+		
+					if (sQuery) {
+						aFilter.push(new Filter("name", FilterOperator.Contains, sQuery));
+					}
+					
+					
+					// filter binding
+					var oList = this.byId("tableVeterinarios");
+					var oBinding = oList.getBinding("items");
+					oBinding.filter(aFilter);
+				},
+		
+		handleRouteMatched: function (oEvent) {
 			var sAppId = "App5fad79356345bf1229e9f919";
 
 			var oParams = {};
@@ -18,7 +90,7 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 
 			} else {
 				if (this.getOwnerComponent().getComponentData()) {
-					var patternConvert = function(oParam) {
+					var patternConvert = function (oParam) {
 						if (Object.keys(oParam).length !== 0) {
 							for (var prop in oParam) {
 								if (prop !== "sourcePrototype" && prop.includes("Set")) {
@@ -44,7 +116,7 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 			}
 
 		},
-		_onRowPress: function(oEvent) {
+		_onRowPress: function (oEvent) {
 
 			var sDialogName = "TesteDialog";
 			this.mDialogs = this.mDialogs || {};
@@ -63,39 +135,148 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 			oDialog.open();
 
 		},
-		onInit: function() {
+		onInit: function () {
 			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			this.oRouter.getTarget("Page1").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
-			
-			var dadosConsultas = null;
-				$.ajax({
-	                "url": "/VETERINARIA-NODE/consultas",
-	                "method": "GET",
-	                "timeout": 0,
-	                async: false,
-	                "headers": {
-	                    "Content-Type": "application/json"
-	                },
-	                success: function (oData) {
-	        			dadosConsultas = oData;
-	        			
-	                },
-	                error: function (oError) {
-		                // var sTargetPos = "";
-		                // sTargetPos = (sTargetPos === "default") ? undefined : sTargetPos;
-		                // sap.m.MessageToast.show("Erro!", {
-		                //     duration: 10000 || 10000,
-		                //     at: sTargetPos,
-		                //     my: sTargetPos
-		                // });
-	                }
-	            });
-	            var oModelConsultas = new JSONModel();
-	            oModelConsultas.setData({
-	            	consultas: dadosConsultas
-	            });
-	            this.getView().setModel(oModelConsultas, "consultas");
 
+			// DONOS
+			var dadosDonos = null;
+			$.ajax({
+				"url": "/VETERINARIA-NODE/donos",
+				"method": "GET",
+				"timeout": 0,
+				async: false,
+				"headers": {
+					"Content-Type": "application/json"
+				},
+				success: function (oData) {
+					dadosDonos = oData;
+
+				},
+				error: function (oError) {
+					// var sTargetPos = "";
+					// sTargetPos = (sTargetPos === "default") ? undefined : sTargetPos;
+					// sap.m.MessageToast.show("Erro!", {
+					//     duration: 10000 || 10000,
+					//     at: sTargetPos,
+					//     my: sTargetPos
+					// });
+				}
+			});
+			var oModelDonos = new JSONModel();
+			oModelDonos.setData({
+				donos: dadosDonos
+			});
+			this.getView().setModel(oModelDonos, "donos");
+
+			// ANIMAIS
+			var dadosAnimais = null;
+			$.ajax({
+				"url": "/VETERINARIA-NODE/animais",
+				"method": "GET",
+				"timeout": 0,
+				async: false,
+				"headers": {
+					"Content-Type": "application/json"
+				},
+				success: function (oData) {
+					dadosAnimais = oData;
+
+				},
+				error: function (oError) {
+					// var sTargetPos = "";
+					// sTargetPos = (sTargetPos === "default") ? undefined : sTargetPos;
+					// sap.m.MessageToast.show("Erro!", {
+					//     duration: 10000 || 10000,
+					//     at: sTargetPos,
+					//     my: sTargetPos
+					// });
+				}
+			});
+			var oModelAnimais = new JSONModel();
+			oModelAnimais.setData({
+				animais: dadosAnimais
+			});
+			this.getView().setModel(oModelAnimais, "animais");
+
+			// CONSULTAS
+			var dadosConsultas = null;
+			$.ajax({
+				"url": "/VETERINARIA-NODE/consultas",
+				"method": "GET",
+				"timeout": 0,
+				async: false,
+				"headers": {
+					"Content-Type": "application/json"
+				},
+				success: function (oData) {
+					dadosConsultas = oData;
+
+				},
+				error: function (oError) {
+					// var sTargetPos = "";
+					// sTargetPos = (sTargetPos === "default") ? undefined : sTargetPos;
+					// sap.m.MessageToast.show("Erro!", {
+					//     duration: 10000 || 10000,
+					//     at: sTargetPos,
+					//     my: sTargetPos
+					// });
+				}
+			});
+			var oModelConsultas = new JSONModel();
+			oModelConsultas.setData({
+				consultas: dadosConsultas
+			});
+			this.getView().setModel(oModelConsultas, "consultas");
+
+			// VETERINARIOS
+			var dadosVeterinarios = null;
+			$.ajax({
+				"url": "/VETERINARIA-NODE/veterinarios",
+				"method": "GET",
+				"timeout": 0,
+				async: false,
+				"headers": {
+					"Content-Type": "application/json"
+				},
+				success: function (oData) {
+					dadosVeterinarios = oData;
+
+				},
+				error: function (oError) {
+					// var sTargetPos = "";
+					// sTargetPos = (sTargetPos === "default") ? undefined : sTargetPos;
+					// sap.m.MessageToast.show("Erro!", {
+					//     duration: 10000 || 10000,
+					//     at: sTargetPos,
+					//     my: sTargetPos
+					// });
+				}
+			});
+			var oModelVeterinarios = new JSONModel();
+			oModelVeterinarios.setData({
+				veterinarios: dadosVeterinarios
+			});
+			this.getView().setModel(oModelVeterinarios, "veterinarios");
+
+			// TAMANHO DE TODOS
+			var lengthVets = dadosVeterinarios.length;
+			var lengthDonos = dadosDonos.length;
+			var lengthAnimais = dadosAnimais.length;
+			var lengthConsultas = dadosConsultas.length;
+
+			var length = {
+				length: {
+					vets: lengthVets,
+					donos: lengthDonos,
+					animais: lengthAnimais,
+					consultas: lengthConsultas
+				}
+			};
+
+			var oModelLength = new JSONModel(length);
+			this.getView().setModel(oModelLength, "length");
+			console.log(length);
 		}
 	});
 }, /* bExport= */ true);
